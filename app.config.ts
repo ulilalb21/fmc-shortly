bb80bd791ffbca748d14c7695bcea91d5aec6671
Ulil Albab <ulil.alb21@gmail.com>
2024-08-26 11:21:36 +0700

    fixes

--- app.config.ts
+++ app.config.ts
@@ -2,5 +2,10 @@ export default defineAppConfig({
   ui: {
     primary: "cyan",
     gray: "neutral",
+    button: {
+      variant: {
+        solid: "hover:bg-{color}-200",
+      },
+    },
   },
 });
